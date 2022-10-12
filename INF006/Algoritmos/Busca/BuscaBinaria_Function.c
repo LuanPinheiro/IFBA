19bd052db8b94b81cd7165515f1566c69120a4ed
Luan <57464923+LuanPinheiro@users.noreply.github.com>
2022-10-12 00:42:55 +0000

    Iniciado Lista 1, questão 2 concluida

---
+++ INF006/Algoritmos/Busca/BuscaBinaria_Function.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define n 10
+
+// Em um vetor "vet[n]" ORDENADO, busca um numero "num", utilizando um método de encontrar o meio do vetor e checar se é maior ou menor que o valor de vet[meio], e fazendo o meio se tornar o inicio/fim da proxima checagem, gerando um novo meio
+
+int BinarySearch(int vetor[], int inicio, int fim, int valor_buscado){
+	if(inicio > fim){
+		return -1; // O valor não foi encontrado
+	}
+
+	meio = (inicio + fim) / 2;
+	
+	if(vetor[meio] == valor_buscado){
+		return meio; // O valor foi encontrado na posição em int que será retornada
+	}
+	else if(vetor[meio] > valor_buscado){
+		return BinarySearch(vetor, inicio, meio-1, valor_buscado); // Será buscado do meio para menos
+	}
+	else{
+		return BinarySearch(vetor, meio+1, fim, valor_buscado); // Será buscado do meio para mais
+	}
+}
---
+++ INF006/Listas/Lista1/q2.c
@@ -0,0 +1,141 @@
+/*Considere uma aplicação para armazenar os seguintes dados de uma pessoa em uma agenda de endereços:
+nome, endereço e telefone. Especifique um TAD para armazenar os dados das pessoas e as operações
+necessárias para inserir, consultar e excluir os dados das pessoas.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define tam 100
+
+typedef struct{
+	char nome[52];
+	char endereco[52];
+	char telefone[52]; // formato "9 9999 9999" sem os espaços
+	int cadastrado;
+}person;
+
+int incluirPessoa(person pessoas[], int qtd);
+int excluirPessoa(person pessoas[], int qtd);
+void consultarPessoa(person pessoas[], int qtd);
+void limparString(char string[]);
+
+int main(){
+	person pessoas[tam];
+	int op, qtd = 0;
+	
+	do{
+		system("clear");
+		printf("%d\n\n", qtd);
+		printf("1. Inserir\n2. Excluir\n3. Consultar\n");
+		printf("(Digite 0 para fechar o programa)\n");
+		scanf("%d", &op);
+		getchar();
+
+		switch(op){
+			case 0: printf("\n\nFECHANDO O PROGRAMA"); return 0; break;
+			case 1: qtd = incluirPessoa(pessoas, qtd); break;
+			case 2: qtd = excluirPessoa(pessoas, qtd); break;
+			case 3: consultarPessoa(pessoas, qtd); break;
+			default: printf("\nOPCAO INVALIDA\n\n");
+		}
+	}while(op);
+}
+
+int incluirPessoa(person pessoas[], int qtd){
+	system("clear");
+	
+	printf("Digite o nome da pessoa: ");
+	fgets(pessoas[qtd].nome, 52, stdin);
+	limparString(pessoas[qtd].nome);
+
+	printf("Digite o endereco da pessoa: ");
+	fgets(pessoas[qtd].endereco, 52, stdin);
+	limparString(pessoas[qtd].endereco);
+
+	printf("Digite o telefone da pessoa: ");
+	fgets(pessoas[qtd].telefone, 52, stdin);
+	limparString(pessoas[qtd].telefone);
+	
+	pessoas[qtd].cadastrado = true;
+	return ++qtd;
+}
+
+int excluirPessoa(person pessoas[], int qtd){
+	char telefone[52];
+	int existe, i, count;
+	
+	do{
+		system("clear");
+		existe = false;
+		printf("Digite um telefone registrado: ");
+		fgets(telefone, 52, stdin);
+		limparString(telefone);
+
+		for(i=0, count=0; count<qtd; i++){
+			if(pessoas[i].cadastrado == true){
+				if(strcmp(telefone, pessoas[i].telefone)==0){
+					existe = true;
+					break;
+				}
+				count++;
+			}
+			if(existe == true){
+				break;
+			}
+		}
+		if(existe == false){
+			printf("\nPessoa nao encontrada\n");
+			getchar();
+		}
+	}while(existe==false);
+
+	pessoas[i].cadastrado = false;
+	qtd--;
+	getchar();
+	
+	return qtd;
+}
+
+void consultarPessoa(person pessoas[], int qtd){
+	char telefone[52];
+	int existe, i, count;
+	
+	do{
+		system("clear");
+		existe = false;
+		printf("Digite um telefone registrado: ");
+		fgets(telefone, 52, stdin);
+		limparString(telefone);
+
+		for(i=0, count=0; count<qtd; i++){
+			if(pessoas[i].cadastrado == true){
+				if(strcmp(telefone, pessoas[i].telefone)==0){
+					existe = true;
+					break;
+				}
+				count++;
+			}
+			if(existe == true){
+				break;
+			}
+		}
+		if(existe == false){
+			printf("\nPessoa nao encontrada\n");
+			getchar();
+		}
+	}while(existe==false);
+
+	printf("\nNome da pessoa: %s", pessoas[i].nome);
+	printf("\nEndereco da pessoa: %s", pessoas[i].endereco);
+	printf("\nTelefone da pessoa: %s\n\n", pessoas[i].telefone);
+	getchar();
+}
+
+// Usada para retirar o '\n' após inserir uma string[] via fgets()
+void limparString(char string[]){
+	for(int i=0;string[i] != '\0';i++)
+		if(string[i] == '\n')
+			string[i] = '\0';
+}
