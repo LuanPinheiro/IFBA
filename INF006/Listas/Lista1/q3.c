3b03245f2274bb2670dff090ab32427cbca7c8b0
LuanPinheiro <luanp.soledade@gmail.com>
2022-10-13 10:17:49 -0300

    Questão 3 Concluída

---
+++ INF006/Listas/Lista1/q3.c
@@ -0,0 +1,142 @@
+/*Considere uma aplicação para armazenar os seguintes dados de carros para uma garagem: placa,
+marca/modelo e cor. Especifique um TAD para armazenar os dados dos carros e as operações necessárias para inserir,
+consultar e excluir os dados das pessoas.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define tam 100
+
+typedef struct{
+	char placa[10];
+	char marca[10];
+	char cor[10];
+	int cadastrado;
+}carros;
+
+int incluirCarro(carros carros[], int qtd);
+int excluirCarro(carros carros[], int qtd);
+void consultarCarro(carros carros[], int qtd);
+void limparString(char string[]);
+
+int main(){
+	carros garagem[tam];
+	int op, qtd = 0;
+	
+	do{
+		system("cls");
+		printf("%d\n\n", qtd);
+		printf("1. Inserir\n2. Excluir\n3. Consultar\n");
+		printf("(Digite 0 para fechar o programa)\n");
+		scanf("%d", &op);
+		getchar();
+
+		switch(op){
+			case 0: printf("\n\nFECHANDO O PROGRAMA"); return 0; break;
+			case 1: qtd = incluirCarro(garagem, qtd); break;
+			case 2: qtd = excluirCarro(garagem, qtd); break;
+			case 3: consultarCarro(garagem, qtd); break;
+			default: printf("\nOPCAO INVALIDA\n\n");
+		}
+	}while(op);
+}
+
+int incluirCarro(carros garagem[], int qtd){
+	system("cls");
+	
+	printf("Digite a placa do carro: ");
+	fgets(garagem[qtd].placa, 10, stdin);
+	limparString(garagem[qtd].placa);
+
+	printf("Digite a marca do carro: ");
+	fgets(garagem[qtd].marca, 10, stdin);
+	limparString(garagem[qtd].marca);
+
+	printf("Digite a cor do carro: ");
+	fgets(garagem[qtd].cor, 10, stdin);
+	limparString(garagem[qtd].cor);
+	
+	garagem[qtd].cadastrado = true;
+	return ++qtd;
+}
+
+int excluirCarro(carros garagem[], int qtd){
+	char placa[10];
+	int existe, i, count;
+	
+	do{
+		system("cls");
+		existe = false;
+		printf("Digite uma placa registrada: ");
+		fgets(placa, 10, stdin);
+		limparString(placa);
+
+		for(i=0, count=0; count<qtd; i++){
+			if(garagem[i].cadastrado == true){
+				if(strcmp(placa, garagem[i].placa)==0){
+					existe = true;
+					break;
+				}
+				count++;
+			}
+			if(existe == true){
+				break;
+			}
+		}
+		if(existe == false){
+			printf("\nCarro nao encontrado\n");
+			getchar();
+		}
+	}while(existe==false);
+
+	garagem[i].cadastrado = false;
+	qtd--;
+    printf("\n(Carro de placa %s excluido)");
+	getchar();
+	
+	return qtd;
+}
+
+void consultarCarro(carros garagem[], int qtd){
+	char placa[10];
+	int existe, i, count;
+	
+	do{
+		system("cls");
+		existe = false;
+		printf("Digite uma placa registrada: ");
+		fgets(placa, 10, stdin);
+		limparString(placa);
+
+		for(i=0, count=0; count<qtd; i++){
+			if(garagem[i].cadastrado == true){
+				if(strcmp(placa, garagem[i].placa)==0){
+					existe = true;
+					break;
+				}
+				count++;
+			}
+			if(existe == true){
+				break;
+			}
+		}
+		if(existe == false){
+			printf("\nCarro nao encontrado\n");
+			getchar();
+		}
+	}while(existe==false);
+
+	printf("\nPlaca do carro: %s", garagem[i].placa);
+	printf("\nMarca do carro: %s", garagem[i].marca);
+	printf("\nCor do carro: %s\n\n", garagem[i].cor);
+	getchar();
+}
+
+// Usada para retirar o '\n' após inserir uma string[] via fgets()
+void limparString(char string[]){
+	for(int i=0;string[i] != '\0';i++)
+		if(string[i] == '\n')
+			string[i] = '\0';
+}
